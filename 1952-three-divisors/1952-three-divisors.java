db1f53e63ea89458b2461a5a5a7c764fa4491806
sunnykumar31 <102225276+sunnykumar31@users.noreply.github.com>
2022-09-08 04:00:31 +0530

    Time: 1 ms (92.10%), Space: 40.9 MB (53.32%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -2,9 +2,12 @@ class Solution {
     public boolean isThree(int n) {
         int count=2;
         int i=2;
-        while(i<=n/2){
+        while(i<=Math.sqrt(n)){
             if(n%i==0){
                 count++;
+                if(n/i>i){
+                    count++;
+                } 
             }
             if(count>3){
                 return false;
