a6c0fd3ab2a5ce47ccfd026b4546dec8348e16a1
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-09-14 14:29:06 +0530

    Time: 19 ms (16.89%), Space: 72.2 MB (43.15%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -7,22 +7,14 @@ class Solution {
         int j=0;
         long product=1;
         int ans=0;
-        int length=0;
         int n=nums.length;
         while(j<n){
             product*=nums[j];
-            if(product<k){
-                length+=1;
-                ans+=length;
-            }
-            else{
                 while(i<=j && product>=k){
                     product/=nums[i];
                     i++;
                 }
                 ans+=(j-i+1);
-                length=(j-i+1);
-            }
             j++;
         }
         return ans;
