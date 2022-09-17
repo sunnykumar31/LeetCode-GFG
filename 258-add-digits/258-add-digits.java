8f9e3875ff1eb82f6e4adf5a6d9f635d1d4e8536
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-09-17 23:48:55 +0530

    Time: 2 ms (74.48%), Space: 41.2 MB (68.86%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        return 1 + (num - 1) % 9;
+        // return 1 + (num - 1) % 9;
+        if ( num == 0 ) return 0;
+	    return num%9 == 0 ? 9 : num%9 ;
     }
 }
