769d3b1e0b7b7d67e5cfcd83e626781375ce8d70
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-09-27 01:27:05 +0530

    Time: 5 ms (41.41%), Space: 41.9 MB (63.95%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -3,9 +3,11 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length-2;i++){
             for(int j=i+1;j<nums.length-1;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[j]- nums[i]==diff && nums[k]-nums[j]==diff){
-                        count++;
+                if(nums[j]- nums[i]==diff){
+                    for(int k=j+1;k<nums.length;k++){
+                        if(nums[k]-nums[j]==diff){
+                            count++;
+                        }
                     }
                 }
             }
