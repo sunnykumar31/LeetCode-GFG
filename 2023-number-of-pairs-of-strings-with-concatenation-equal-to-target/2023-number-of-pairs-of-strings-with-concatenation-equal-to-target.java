75174e223f4b35fac08fcc0639d042121fbb50f5
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-10-13 17:05:45 +0530

    Time: 37 ms (44.63%), Space: 42.3 MB (92.56%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -3,26 +3,27 @@ class Solution {
         //O(n2)
         int count=0;
         int n=nums.length;
-//         for(int i=0;i<n-1;i++){
-//             for(int j=i+1;j<n;j++){
-//                 if(target.equals(nums[i]+nums[j])){
-//                     count++;
-//                     if(nums[i].equals(nums[j])){
-//                           count++;
-//                     }                      
-//                 }
-//             }
-//         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(target.equals(nums[i]+nums[j]) && i!=j){
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(target.equals(nums[i]+nums[j])){
                     count++;
-                    // if(nums[i].equals(nums[j])){
-                    //       count++;
-                    // }   
+                                          
+                }
+                if(target.equals(nums[j]+nums[i])){
+                          count++;
                 }
             }
         }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(target.equals(nums[i]+nums[j]) && i!=j){
+        //             count++;
+        //             // if(nums[i].equals(nums[j])){
+        //             //       count++;
+        //             // }   
+        //         }
+        //     }
+        // }
         return count;
     }
 }
