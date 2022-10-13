d16a1d7c122767be1b04dd36b1cc131e8374f64d
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-10-13 16:59:46 +0530

    Time: 84 ms (13.22%), Space: 61.9 MB (28.93%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -3,20 +3,20 @@ class Solution {
         //O(n2)
         int count=0;
         int n=nums.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                String str="";
-                str=nums[i]+nums[j];
-                if(str.equals(target)){
+                if(target.equals(nums[i]+nums[j])){
                     count++;
+                    // if(nums[i].equals(nums[j])){
+                    //       count++;
+                    // }
+                      
                 }
             }
         }
         for(int i=n-1;i>=0;i--){
             for(int j=i-1;j>=0;j--){
-                String str="";
-                str=nums[i]+nums[j];
-                if(str.equals(target)){
+                if(target.equals(nums[i]+nums[j])){
                     count++;
                 }
             }
