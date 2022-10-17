c5f515f546a974aed50ec63f43a19be7f3b9c22a
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-10-17 17:00:24 +0530

    Time: 570 ms (5.01%), Space: 117.5 MB (50.78%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int ar[]=new int[points.length];
+        double ar[]=new double[points.length];
         int close[][]=new int[k][2];
         for(int i=0;i<points.length;i++){
             // ar[i]=(int)Math.pow(points[i][0],2)+(int)Math.pow(points[i][1],2);
-            ar[i]=points[i][0]*points[i][0] +points[i][1]*points[i][1];
+            ar[i]=Math.sqrt(points[i][0]*points[i][0] +points[i][1]*points[i][1]);
         }
         // System.out.println(ar[0]+" "+ar[1]);
         for(int j=0;j<k;j++){
-            int min=Integer.MAX_VALUE;
+            double min=Integer.MAX_VALUE;
             int index=0;
             for(int i=0;i<points.length;i++){
                 if(min>ar[i]){
