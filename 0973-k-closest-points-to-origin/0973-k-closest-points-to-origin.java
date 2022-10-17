930ce03f8b6d85b9ca52a1513bdc72a2492c61c6
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-10-17 11:48:59 +0530

    Time: 697 ms (5.01%), Space: 118.4 MB (25.63%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int ar[]=new int[points.length];
+        int close[][]=new int[k][2];
+        for(int i=0;i<points.length;i++){
+            ar[i]=(int)Math.pow(points[i][0],2)+(int)Math.pow(points[i][1],2);
+        }
+        // System.out.println(ar[0]+" "+ar[1]);
+        for(int j=0;j<k;j++){
+            int min=Integer.MAX_VALUE;
+            int index=0;
+            for(int i=0;i<points.length;i++){
+                if(min>ar[i]){
+                    min=ar[i];
+                    index=i;
+                }
+            }
+            close[j][0]=points[index][0];
+            close[j][1]=points[index][1];
+            ar[index]=Integer.MAX_VALUE;
+        }
+        return close;
+    }
+}
