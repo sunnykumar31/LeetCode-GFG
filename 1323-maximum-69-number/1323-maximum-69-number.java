e2594994aff8edad768d2ad742e2d8f636b513ff
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-10-29 01:55:55 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (33.07%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,13 +1,28 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s=Integer.toString(num);
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='6'){
-                s=s.replaceFirst("6","9");
-                return Integer.valueOf(s); 
-            }
-        }
-        return num;
+        // String s=Integer.toString(num);
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='6'){
+        //         s=s.replaceFirst("6","9");
+        //         return Integer.valueOf(s); 
+        //     }
+        // }
+        // return num;
         
+        // Math //remainder//3*100xx to 6*100xx making it 900xx
+        
+        int temp=num;
+        int max=0;
+        int pos=1;
+        
+        while(temp!=0){
+            int rem=temp%10;
+            if(rem==6){
+                max=3*pos;
+            }
+            pos*=10;
+            temp/=10;
+        }
+        return num+max; 
     }
 }
