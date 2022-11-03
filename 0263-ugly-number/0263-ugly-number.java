84d954db5e8be1f69c0c09dde1ca24e5c7916146
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-03 21:35:21 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (86.19%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,27 +1,60 @@
 class Solution {
     public boolean isUgly(int n) {
+        // my approch
+        
+        // if(n==0){
+        //     return false;
+        // }
+        // int f=1;
+        // while(f==1){
+        //     f=0;
+        //     if(n%2==0){
+        //         n/=2;
+        //         f=1;
+        //     }
+        //     if(n%3==0){
+        //         n/=3;
+        //         f=1;
+        //     }
+        //     if(n%5==0){
+        //         n/=5;
+        //         f=1;
+        //     }
+        // }
+        // if(n==1){
+        //     return true;
+        // }
+        // return false;
+        
+        
+        
+        // learned from submission
         if(n==0){
             return false;
         }
-        int f=1;
-        while(f==1){
-            f=0;
-            if(n%2==0){
-                n/=2;
-                f=1;
-            }
-            if(n%3==0){
-                n/=3;
-                f=1;
-            }
-            if(n%5==0){
-                n/=5;
-                f=1;
-            }
+        
+        while(n%30==0){
+            n/=30;
         }
-        if(n==1){
-            return true;
+        while(n%15==0){
+            n/=15;
         }
-        return false;
+        while(n%10==0){
+            n/=10;
+        }
+        while(n%6==0){
+            n/=6;
+        }
+        while(n%5==0){
+            n/=5;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+        
+        return n==1;
     }
 }
