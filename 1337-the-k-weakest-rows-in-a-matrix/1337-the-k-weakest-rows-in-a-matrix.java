505ee5fcc4894bb0a2c42eff2028564d3b76d9a8
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-05 00:32:56 +0530

    Time: 6 ms (43.04%), Space: 48.3 MB (73.82%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -4,18 +4,16 @@ class Solution {
         int m=mat[0].length;
         int ar[]=new int[n];
         for(int i=0;i<n;i++){
-            int l=0;
-            int r=m;
-            while(l<r){
-                int mid=(l+r)/2;
-                if(mat[i][mid]==1){
-                    l=mid+1;
+            int sum=0;
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1){
+                    sum++;
                 }
                 else{
-                    r=mid;
+                    break;
                 }
             }
-            ar[i]=l*1000+i;
+            ar[i]=sum*1000+i;
         }
         Arrays.sort(ar);
         int w_to_s[]=new int[k];
