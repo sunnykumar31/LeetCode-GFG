7c6ad3f00061323df2382045ddcb106e874f706c
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-05 22:23:49 +0530

    Time: 2 ms (97.88%), Space: 43.7 MB (64.49%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,13 +1,25 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
         //brute forces
-        int count=0;
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(Math.abs(nums[i]-nums[j])==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        //one loop approch with extra constant space
+        
+        int ar[]=new int[101];
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(Math.abs(nums[i]-nums[j])==k){
-                    count++;
-                }
-            }
+            ar[nums[i]]++;
+        }
+        int count=0;
+        for(int i=0;i+k<101;i++){
+            count+=ar[i]*ar[i+k];
         }
         return count;
     }
