d7e08a71c81834f124cf61f58fb6e8b487920a29
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-06 18:04:19 +0530

    Time: 7 ms (99.57%), Space: 55.3 MB (47.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -18,14 +18,19 @@ class Solution {
         }
         List<Integer> l=new ArrayList<>();
         for(int i=0;i<itr.length-1;i++){
+            // if y of current row is greater than x of next row then overlapping occur
             if(itr[i][1]>=itr[i+1][0]){
+                // if x of current row is less than or equal than copy the value of x in next row;
                 if(itr[i][0]<=itr[i+1][0]){
                     itr[i+1][0]=itr[i][0];
                 }
+                //if y of current row is greater than or equal than copy the value of y in next row;
                 if(itr[i][1]>=itr[i+1][1]){
                     itr[i+1][1]=itr[i][1];
                 }
             }
+            // if overlaping is not occur 
+            
             else{
                 l.add(itr[i][0]);
                 l.add(itr[i][1]);
