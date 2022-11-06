53fe34efa0c81d4c0f6f892bc8c9f703f26c12f5
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-06 22:40:10 +0530

    Time: 7 ms (13.38%), Space: 47.9 MB (34.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,64 @@
 class Solution {
-    public int[][] insert(int[][] itr1, int[] newitr) {
+    public int[][] insert(int[][] itr_actual, int[] newitr) {
         // List<Integer<Integer>> ans=new ArrayList<>();
-        int len=itr1.length;
+        int len=itr_actual.length;
         int ari[]=new int[len+1];
         int arj[]=new int[len+1];
-        ari[0]=newitr[0];
-        arj[0]=newitr[1];
         // coping the in  ari of 0 col element and arj of col 1
+        int index_i=0;
+        int index_j=0;
+        int f1=0;
+        int f2=0;
+        //for col 0 adding newitr element
         for(int i=0;i<len;i++){
-            ari[i+1]=itr1[i][0];
-            arj[i+1]=itr1[i][1];
+            if(itr_actual[i][0]<newitr[0]){
+                ari[i]=itr_actual[i][0];
+            }
+            else{
+                ari[i]=newitr[0];
+                index_i=i;
+                f1=1;
+                break;
+            }
         }
-        // sorting col0 and col1;
-        Arrays.sort(ari);
-        Arrays.sort(arj);
+        if(f1==0){
+            ari[len]=newitr[0];
+        }
+        else{
+            for(int i=index_i;i<len;i++){
+                ari[i+1]=itr_actual[i][0];
+            }
+        }
+        
+        // System.out.println(ari[len]);
+        //for col 1 adding newitr element
+        for(int i=0;i<len;i++){
+            if(itr_actual[i][0]<newitr[1]){
+                arj[i+index_j]=itr_actual[i][1];
+            }
+            else{
+                arj[i+index_j]=newitr[1];
+                index_j=i;
+                f2=1;
+                break;
+            }
+        }
+        if(f2==0){
+            arj[len]=newitr[1];
+        }
+        else{
+            for(int i=index_j;i<len;i++){
+                arj[i+1]=itr_actual[i][1];
+            }
+        }
+        //copying all the 
         int itr[][]=new int[len+1][2];
-        // then coping sorted element in sorted array
         for(int i=0;i<len+1;i++){
             itr[i][0]=ari[i];
             itr[i][1]=arj[i];
+            // System.out.println(itr[i][0]+" "+itr[i][1]);
         }
+        // System.out.println(itr[len][1])
         List<Integer> l=new ArrayList<>();
         for(int i=0;i<len;i++){
             // if y of current row is greater than x of next row then overlapping occur
