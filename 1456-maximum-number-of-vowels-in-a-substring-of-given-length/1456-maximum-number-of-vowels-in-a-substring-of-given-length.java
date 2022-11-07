70e229abf350ffbd242891dc0849c7b58626e58b
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-07 22:08:11 +0530

    Time: 14 ms (91.04%), Space: 48.2 MB (25.76%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,10 +1,46 @@
 class Solution {
     public int maxVowels(String s, int k) {
+        //it don't feel that this code is effecient;
+        // int count=0;
+        // String vowel="aeiou";
+        // for(int i=0;i<k;i++){
+        //     String c=s.substring(i,i+1);
+        //     if(vowel.contains(c)){
+        //         count++;
+        //     }
+        // }
+        // int newcount=count;
+        // int post=k;
+        // int pre=0;
+        // while(post<s.length()){
+        //     String pre_char=s.substring(pre,pre+1);
+        //     if(vowel.contains(pre_char)){
+        //         newcount--;
+        //     }
+        //     String post_char=s.substring(post,post+1);
+        //     if(vowel.contains(post_char)){
+        //         newcount++;
+        //     }
+        //     pre++;
+        //     post++;
+        //     if(newcount>count){
+        //         count=newcount;
+        //     }
+        // }
+        // return count;
+        
+        //by the help of frequency array
+        int len=s.length();
+        boolean vowel[]=new boolean[len];
+        for(int i=0;i<len;i++){
+            char c=s.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                vowel[i]=true;
+            }
+        }
         int count=0;
-        String vowel="aeiou";
         for(int i=0;i<k;i++){
-            String c=s.substring(i,i+1);
-            if(vowel.contains(c)){
+            if(vowel[i]){
                 count++;
             }
         }
@@ -12,12 +48,10 @@ class Solution {
         int post=k;
         int pre=0;
         while(post<s.length()){
-            String pre_char=s.substring(pre,pre+1);
-            if(vowel.contains(pre_char)){
+            if(vowel[pre]){
                 newcount--;
             }
-            String post_char=s.substring(post,post+1);
-            if(vowel.contains(post_char)){
+            if(vowel[post]){
                 newcount++;
             }
             pre++;
@@ -27,5 +61,6 @@ class Solution {
             }
         }
         return count;
+        
     }
 }
