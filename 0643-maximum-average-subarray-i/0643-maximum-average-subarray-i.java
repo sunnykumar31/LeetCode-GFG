b43e8cf10751b1bab2476b4545debaf8ed0403b5
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-07 13:42:04 +0530

    Time: 5 ms (90.43%), Space: 102.6 MB (81.15%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,30 +1,51 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int i=0;
-        int j=nums.length;
-        int st=0;
-        int max=Integer.MIN_VALUE;
-        int sum=0;
-        int divisor=k;
-        while(i<j){
-            if(k>0){
-                sum+=nums[i];
-                k--;
-                i++;
-            }
-            else{
-                if(sum>max){
-                    max=sum;
-                }
-                sum-=nums[st];
-                k++;
-                st++;
+        // 1st approch 
+        // int i=0;
+        // int j=nums.length;
+        // int st=0;
+        // int max=Integer.MIN_VALUE;
+        // int sum=0;
+        // int divisor=k;
+        // while(i<j){
+        //     if(k>0){
+        //         sum+=nums[i];
+        //         k--;
+        //         i++;
+        //     }
+        //     else{
+        //         if(sum>max){
+        //             max=sum;
+        //         }
+        //         sum-=nums[st];
+        //         k++;
+        //         st++;
+        //     }
+        // }
+        // if(sum>max){
+        //     max=sum;
+        // }
+        // double val=(double)max/divisor;
+        // return val;
+        
+        //2nd approch
+        int currsum=0;
+        for(int i=0;i<k;i++){
+            currsum+=nums[i];
+        }
+        int usesum=currsum;
+        int post=k;
+        int pre=0;
+        int len=nums.length;
+        while(post<len){
+            usesum-=nums[pre];
+            usesum+=nums[post];
+            pre++;
+            post++;
+            if(usesum>currsum){
+                currsum=usesum;
             }
         }
-        if(sum>max){
-            max=sum;
-        }
-        double val=(double)max/divisor;
-        return val;
+        return (double)currsum/k;
     }
 }
