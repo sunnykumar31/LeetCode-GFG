a0a0ad9c241efe4ba5ed50756e954722d01ca3db
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-12 19:50:36 -0800

    Time: 2 ms (100.00%), Space: 58.3 MB (82.58%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -5,6 +5,7 @@ class Solution {
         int i=0;
         int j=0;
         int n=nums.length;
+        //finding max and min element index;
         for(int k=0;k<n;k++){
             if(nums[k]>max){
                 max=nums[k];
@@ -15,26 +16,15 @@ class Solution {
                 i=k;
             }
         }
-        if(j>=i){
-            // if(j>=n/2){
-                int p=n-j+i+1;
-                int q=n-i;
-                // return Math.min(p,q);
-            // }
-            // else{
-                // return j+1;
-            // }
-            return Math.min(Math.min(p,q),j+1);
+        if(j>=i){ //if max index element is greater than smaller index element then 
+            int p=n-j+i+1;// deleting elemnet from last and first ex [2,10,7,5,4,1,8,6]
+            int q=n-i;    // deleting element from last ex [2,8,7,5,4,1,10,6]
+            return Math.min(Math.min(p,q),j+1); //for j+1  ex [-1,-53,93,-42,37,94,-97,82,46,42,99,56,-76,-66,-67,-13,10,66,85,-28]
+
         }
         else{ 
-            // if(i>=n/2){
-                int p=n-i+j+1;
-                int q=n-j;
-                // return Math.min(p,q);
-            // }
-            // else{
-                // return i+1;
-            // }
+            int p=n-i+j+1;
+             int q=n-j;
             return Math.min(Math.min(p,q),i+1);
         }
     }
