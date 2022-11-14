c30ff1d8f288ab98ef57096ec2b77dc058d331be
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-14 00:30:34 -0800

    Time: 21 ms (98.19%), Space: 107.5 MB (14.44%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,18 +1,53 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int i=0;
-        int j=nums.length-1;
-        int sum1=0;
+        // by the sorting method it is easy
+        // Arrays.sort(nums);
+        // int i=0;
+        // int j=nums.length-1;
+        // int sum1=0;
+        // int max=0;
+        // while(i<j){
+        //     sum1=nums[i]+nums[j];
+        //     if(sum1>max){
+        //         max=sum1;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // return max;
+        
+        //by frequency method
+        
         int max=0;
-        while(i<j){
-            sum1=nums[i]+nums[j];
-            if(sum1>max){
-                max=sum1;
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
             }
-            i++;
-            j--;
         }
-        return max;
+        int ar[]=new int[max+1]; //we ar going to use index as element in sorted form 
+        for(int i=0;i<nums.length;i++){
+            ar[nums[i]]++;
+        }
+        int i=0;
+        int j=ar.length-1;
+        int fmax=0;
+        while(i<=j){
+            if(ar[i]==0){
+                i++;
+                continue;
+            }
+            if(ar[j]==0){
+                j--;
+                continue;
+            }
+            if(ar[i]!=0 && ar[j]!=0){
+                if(i+j>fmax){
+                    fmax=i+j;
+                }
+                ar[i]--;
+                ar[j]--;
+            }
+        }
+        return fmax;
     }
 }
