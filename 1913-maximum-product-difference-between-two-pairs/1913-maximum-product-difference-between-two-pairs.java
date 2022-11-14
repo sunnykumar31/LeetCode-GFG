8ca6f71ddb6a7fed691795ca71f9a07efb13e152
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-13 23:52:35 -0800

    Time: 5 ms (88.99%), Space: 53.7 MB (63.95%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -12,20 +12,33 @@ class Solution {
         int max2=Integer.MIN_VALUE;
         int min1=Integer.MAX_VALUE;
         int min2=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=max1){
-                max2=max1;
-                max1=nums[i];
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>=max1){
+        //         max2=max1;
+        //         max1=nums[i];
+        //     }
+        //     else if(max1>=nums[i] && max2<nums[i] && nums[i]<=max1){
+        //         max2=nums[i];
+        //     }
+        //     if(nums[i]<=min1){
+        //         min2=min1;
+        //         min1=nums[i];
+        //     }
+        //     else if(min1<=nums[i] && min2>nums[i] && nums[i]>=min1){
+        //         min2=nums[i];
+        //     }
+        // }
+        
+        
+        for(int num : nums) {
+            if(num > max1 || num > max2) { // if num is greater than any of the two, update our values
+                max1 = Math.max(max1, max2);
+                max2 = num;
             }
-            else if(max1>=nums[i] && max2<nums[i] && nums[i]<=max1){
-                max2=nums[i];
-            }
-            if(nums[i]<=min1){
-                min2=min1;
-                min1=nums[i];
-            }
-            else if(min1<=nums[i] && min2>nums[i] && nums[i]>=min1){
-                min2=nums[i];
+            
+            if(num < min1 || num < min2) { //if num is smaller than any of the two, update our values
+                min1 = Math.min(min1, min2);
+                min2 = num;
             }
         }
         return max1*max2-min1*min2;
