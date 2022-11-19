f3cdeec00eec2b227aa2996445b6d1894576c751
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-19 12:08:10 -0800

    Time: 22 ms (34.67%), Space: 47 MB (20.69%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,30 +1,41 @@
 class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
-        int len=nums.length;
-        if(len==1){
-            return 0;
+        // median approch
+        // int len=nums.length;
+        // if(len==1){
+        //     return 0;
+        // }
+        // if(len%2==0){
+        //     int index=len/2;
+        //     int median1=(nums[index]+nums[index-1])/2;
+        //     int median2=median1+1;
+        //     int count1=0;
+        //     int count2=0;
+        //     for(int i=0;i<len;i++){
+        //         count1+=Math.abs(median1-nums[i]);
+        //         count2+=Math.abs(median2-nums[i]);
+        //     }
+        //     return Math.min(count1,count2);
+        // }
+        // else{
+        //     int index=len/2;
+        //     int median1=nums[index];
+        //     int count1=0;
+        //     for(int i=0;i<len;i++){
+        //         count1+=Math.abs(median1-nums[i]);
+        //     }
+        //     return count1;
+        // }
+        
+        int i =0;
+        int j = nums.length - 1;
+        int count = 0;
+        while (i < j){
+            count += nums[j] - nums[i];
+            i++;
+            j--;
         }
-        if(len%2==0){
-            int index=len/2;
-            int median1=(nums[index]+nums[index-1])/2;
-            int median2=median1+1;
-            int count1=0;
-            int count2=0;
-            for(int i=0;i<len;i++){
-                count1+=Math.abs(median1-nums[i]);
-                count2+=Math.abs(median2-nums[i]);
-            }
-            return Math.min(count1,count2);
-        }
-        else{
-            int index=len/2;
-            int median1=nums[index];
-            int count1=0;
-            for(int i=0;i<len;i++){
-                count1+=Math.abs(median1-nums[i]);
-            }
-            return count1;
-        }
+        return count;
     }
 }
