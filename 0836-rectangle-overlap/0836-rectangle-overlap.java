0c1747791ce31c0d325e3f01aac35dccbbe2232a
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-26 09:18:41 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (32.92%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -8,5 +8,11 @@ class Solution {
             return false;
         }
         return true;
+        // return 
+        //     rec1[0] < rec2[2] &&
+        //     rec1[1] < rec2[3] &&
+        //     rec1[2] > rec2[0] &&
+        //     rec1[3] > rec2[1]
+        // ;
     }
 }
