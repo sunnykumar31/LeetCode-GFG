a3639685a3a10b71679e259eaced0499349eb63f
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-26 09:17:57 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (5.06%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int x1=(int)Math.max(rec1[0],rec2[0]);
+        int y1=(int)Math.max(rec1[1],rec2[1]);
+         int x2=(int)Math.min(rec1[2],rec2[2]);
+        int y2=(int)Math.min(rec1[3],rec2[3]);
+        if((x2-x1)<=0 || (y2-y1)<=0){//means both rectangle don't intersects
+            return false;
+        }
+        return true;
+    }
+}
