bf89f61e612a70a742d214b044d8a35a3887cf56
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-11-28 22:54:09 +0530

    Time: 93 ms (92.19%), Space: 149.3 MB (60.82%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -4,33 +4,33 @@ class Solution {
         int len_l=0;
         for(int i=0;i<m.length;i++){
             if(m[i][0]>len_w){
-                len_w=m[i][0];
+                len_w=m[i][0]; // finding maximum of winner 
             }
             if(m[i][1]>len_l){
-                len_l=m[i][1];
+                len_l=m[i][1];  //finding minimum of losser
             }
         }
-        len_w++;
+        len_w++; // just for array length 
         len_l++;
         int ar_w[]=new int[len_w];
         int ar_l[]=new int[len_l];
         for(int i=0;i<m.length;i++){
-            ar_w[m[i][0]]++;
-            ar_l[m[i][1]]++;
+            ar_w[m[i][0]]++;    // making frequency array of winner
+            ar_l[m[i][1]]++;    // making frequency arrray of looser
         }
         List<Integer> list_w=new ArrayList<>();
         List<Integer> list_l=new ArrayList<>();
         int i=1;
         for(;i<len_w;i++){
-            if(len_l-1<i || ar_l[i]==0){
-                if(ar_w[i]!=0)
+            if(len_l-1<i || ar_l[i]==0){  // if looser length length is less than winner or not present in looser array then its mean that it will never loose
+                if(ar_w[i]!=0)  //and also in winner array  that ele must be present.
                     list_w.add(i);
             }
-            else if(ar_l[i]==1){
+            else if(ar_l[i]==1){ // if one loss only
                 list_l.add(i);
             }
         }
-        if(len_l>len_w){
+        if(len_l>len_w){ // if for winner array is less than looser array 
             while(i<len_l){
                 if(ar_l[i]==1){
                     list_l.add(i);
