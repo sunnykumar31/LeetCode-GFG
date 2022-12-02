5a8f45a6edc9eb9a9475a22c0f45aeef58c819f1
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-03 01:19:01 +0530

    Time: 17 ms (83.26%), Space: 54.5 MB (66.96%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -2,18 +2,18 @@ class Solution {
     public boolean closeStrings(String word1, String word2) {
         int freq1[]=new int [26];
         int freq2[]=new int [26];
-        for(int i=0;i<word1.length();i++){
+        for(int i=0;i<word1.length();i++){ //frequency array
             freq1[word1.charAt(i)-'a']++;
         }
         for(int i=0;i<word2.length();i++){
             freq2[word2.charAt(i)-'a']++;
         }
-        for(int i=0;i<26;i++){
+        for(int i=0;i<26;i++){  // checking if character is only in one of them then return true;
             if((freq1[i]==0 && freq2[i]!=0) || (freq1[i]!=0 && freq2[i]==0)){
                 return false;
             }
         }
-        for(int i=0;i<26;i++){
+        for(int i=0;i<26;i++){  // if existing character in both then checking frequency will be same
             int f=0;
             for(int j=0;j<26;j++){
                 if(freq1[i]==freq2[j]){
