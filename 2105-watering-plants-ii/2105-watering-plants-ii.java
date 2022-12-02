ff31d0615a96b3d27f000ba527cc5391faf2eccc
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-03 01:21:43 +0530

    Time: 4 ms (98.20%), Space: 57.5 MB (85.59%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.java
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -6,21 +6,21 @@ class Solution {
         int i=0;
         int j=plants.length-1;
         while(i<=j){
-            if(i==j){
+            if(i==j){ // when both of index is same than check if both of water is less than required than we have to refill the pot 
                 if(plants[i]>capA && plants[i]>capB){
                     count++;
                 }
                 break;
             }
-            if(plants[i]>capA){
+            if(plants[i]>capA){ // water is less as per requirement then  refile the pot
                 count++;
                 capA=capacityA;
             }
             if(plants[j]>capB){
                 count++;
-                capB=capacityB;
+                capB=capacityB;// same as capB 
             }
-            capA-=plants[i];
+            capA-=plants[i]; // after giving water ,remaming water in pot
             capB-=plants[j];
             i++;
             j--;
