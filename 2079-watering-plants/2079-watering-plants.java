d55448c518c1b4ee6556d5db6a9e5d0adcf3a9f7
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-03 01:27:21 +0530

    Time: 3 ms (35.11%), Space: 42.7 MB (69.67%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.java
+++ 2079-watering-plants/2079-watering-plants.java
@@ -3,20 +3,11 @@ class Solution {
         int count=0;
         int cap=capacity;
         for(int i=0;i<plants.length;i++){
-            // if(plants[i]<=cap){
-            //     count++;
-            //     cap-=plants[i];
-            // }
-            // else{
-            //     count=count+i+i; 
-            //     cap=capacity;
-            //     i--;
-            // }
             if(plants[i]>cap){
-                count=count+2*i;
+                count=count+2*i; //us index k piche ja kr river se pani lane ka then river se pani lekr ane ka duri 
                 cap=capacity;
             }
-            count++;
+            count++; // requirement fulfill kr k aage ek step bdhne ka;
             cap-=plants[i];
         }
         return count;
