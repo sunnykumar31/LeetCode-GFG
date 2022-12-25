8e1628d8410c473b278243ed40e3846bb4d112eb
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-25 12:47:30 +0530

    Time: 3 ms (100.00%), Space: 42.9 MB (85.15%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -8,15 +8,17 @@ class Solution {
             nums[i]+=nums[i-1];
         }
         for(int i=0;i<lq;i++){
+            if(queries[i]>=nums[ln-1]){
+                ans[i]=ln;
+                continue;
+            }
             for(int j=0;j<ln;j++){
                 if(queries[i]<nums[j]){
                     ans[i]=j;
                     break;
                 }
             }
-            if(queries[i]>=nums[ln-1]){
-                ans[i]=ln;
-            }
+            
         }
         return ans;
     }
