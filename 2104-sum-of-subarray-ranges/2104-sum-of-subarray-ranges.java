88e4526ee3554e3eab8f96fc5e8f8e1dabb8b9bb
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-26 15:52:05 +0530

    Time: 24 ms (83.35%), Space: 42.1 MB (87.78%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,24 +1,17 @@
 class Solution {
-    public long subArrayRanges(int[] A) {
+    public long subArrayRanges(int[] nums) {
         //Without stack and n^2
-        // long sum=0;
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=i;j<nums.length;j++){
-        //         sum=sum+(long)(Math.max(nums[i],nums[j])-Math.min(nums[i],nums[j]));
-        //     }
-        // }
-        // return sum;
-        
-        
-        long res = 0;
-        for (int i = 0; i < A.length; i++) {
-            int max = A[i], min = A[i];
-            for (int j = i; j < A.length; j++) {
-                max = Math.max(max, A[j]);
-                min = Math.min(min, A[j]);
-                res += max - min;
+        long sum=0;
+        for(int i=0;i<nums.length;i++){
+            int max=nums[i],min=nums[i];
+            for(int j=i;j<nums.length;j++){
+                max=Math.max(max,nums[j]);
+                min=Math.min(min,nums[j]);
+                sum+=max-min;
+                
             }
         }
-        return res;
+        return sum;
+        
     }
 }
