d3a963a6802446b8056d389b603952076a87272a
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-26 13:08:43 +0530

    Time: 2 ms (92.42%), Space: 42.7 MB (89.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // 2 loop  not appropriate approch
+//         int l=nums.length;
+//         if(l==1){
+//             return true;
+//         }
+//         if(nums[0]==0){
+//             return false;
+//         }
+//         for(int i=0;i<l-1;i++){
+//             int j=nums[i];
+//             while(j<l){
+//                 if(j==l-1){
+//                     return true;
+//                 }
+//                 if(nums[j]==0){
+//                     break;
+//                 }
+//                 j+=nums[j];
+//             }
+//             if(j>=l-1){
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+        //  int boundary = 0;
+        //  for(int i =0;i<=boundary;i++){
+        //      boundary = Math.max(boundary,i+nums[i]);
+        //      if(boundary >=nums.length-1)
+        //      return true;
+        //  } 
+        // return false;
+        
+        int reach=0;
+        for(int i=0;i<nums.length;i++){
+            if(reach<i){
+                return false;
+            }
+            reach=Math.max(reach,i+nums[i]);
+            if(reach>=nums.length-1) return true;
+        }
+        return true;
+    }
+}
