87ee837c2c87934bafd9dbdd86b44e6d7ecc3532
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2022-12-28 00:46:08 +0530

    Time: 3 ms (91.96%), Space: 42.4 MB (73.23%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -21,5 +21,7 @@ class Solution {
             }
         }
         return true;
+        
+        //Explantion agar stack empty to hai to phle usme insert krenge agr empty nhi hai to stack k top element se poped k top element se compare krenge agr dono equal hai to mtlb mujhe use pop krna hai agr aisa nhi hota hai to hame push ko aage badhate rhna hai ,ek point pushed array agr empty ho gya mtlb sara valued push hoa gya then agr top value of stack nhi match krta hai poped element se then ye validate nhi hai, false hai 
     }
 }
