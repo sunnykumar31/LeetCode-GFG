c6f7e10ff5bfb8b4a2e0fcae9870817eea022538
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-01-02 00:28:03 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (68.05%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -2,28 +2,20 @@ class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
         int x=coordinates[1][0]-coordinates[0][0];
         int y=coordinates[1][1]-coordinates[0][1];
-        float m=(float)x/y;
-        // System.out.println(m);
-        int f=0;
+        float m=(float)y/x;
         for(int i=2;i<coordinates.length;i++){
             int p=coordinates[i][0]-coordinates[i-1][0];
             int q=coordinates[i][1]-coordinates[i-1][1];
-            float m1=(float)p/q;
-            if(y==0 && q==0){
-                
-            }
-            else if(m1==m){
-                
-            }
+            float m1=(float)q/p;
+            if(x==0 && p==0){}
+            else if(m1==m){}
             else{
-                // f=1;
-                // break;
                 return false;
             }
         }
-        // if(f==0){
-        //     return true;
-        // }
+        // System.out.println(2.0/0);
         return true;
+        
+        
     }
 }
