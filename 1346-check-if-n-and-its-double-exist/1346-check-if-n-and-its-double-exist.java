17f4f282c48e5cafb28b1920565980420d9d17e9
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-01-07 00:38:28 +0530

    Time: 3 ms (64.72%), Space: 43.5 MB (36.10%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         //n2 approch
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if((arr[i]==(2*arr[j])) ||(arr[j]==(2*arr[i]))){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        
+        //by haset TC(n);
+        HashSet<Integer> hs=new HashSet<>();
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if((arr[i]==(2*arr[j])) ||(arr[j]==(2*arr[i]))){
-                    return true;
-                }
-            }
+            if(hs.contains(arr[i]*2) || (arr[i]%2==0 && hs.contains((arr[i])/2))) return true;
+            hs.add(arr[i]);
         }
         return false;
+        //explantion if arr contains twice of element or if arr element if even and its half contin then also return ture 
     }
 }
