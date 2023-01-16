29376aac833a0cb1c46cacb739abdc2471d3331f
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-01-17 00:29:41 +0530

    Time: 70 ms (34.61%), Space: 42.6 MB (71.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ar[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ar[0]=i;
+                    ar[1]=j;
+                    return ar;
+                }
+            }
+        }
+        return ar;
+    }
+}
