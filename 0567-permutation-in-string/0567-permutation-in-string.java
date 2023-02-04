44e3185125bc8c9a906220cade9a96f062b13fb1
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-02-04 14:45:44 +0530

    Time: 915 ms (14.79%), Space: 42.7 MB (41.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int l1=s1.length();
         int l2=s2.length();
+        char[] ch1=s1.toCharArray();
+        Arrays.sort(ch1);
         int i=0;
         while(i+l1<=l2){
-            if(permutaion(s2.substring(i,i+l1),s1)){
+            if(permutaion(ch1,s2.substring(i,i+l1))){
                 return true;
             }
             i++;
         }
         return false;
     }
-    public static boolean permutaion(String s1,String s2){
+    public static boolean permutaion(char ch1[],String s2){
         // System.out.println(s1+" "+s2);
-        char[] ch1=s1.toCharArray(); 
         char[] ch2=s2.toCharArray();
-        Arrays.sort(ch1);
         Arrays.sort(ch2);
         for(int i=0;i<ch1.length;i++){
             if(ch1[i]!=ch2[i]){
