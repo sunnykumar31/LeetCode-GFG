a22554362ee2c867d0a953e630e7bfbe580d13f1
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-02-07 13:57:18 +0530

    Time: 4 ms (100.00%), Space: 51.7 MB (62.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -3,37 +3,34 @@ class Solution {
         if(fruits.length==1) return 1;
         int bskt1=fruits[0];
         int bskt2=fruits[1];
-        int lft=0;
+        int left=0;
         int rgt=2;
         int max=Integer.MIN_VALUE;
         while(rgt<fruits.length){
             if(fruits[rgt]!=bskt1 && fruits[rgt]!=bskt2){
-                max=Math.max(max,rgt-lft);
+                max=Math.max(max,rgt-left);
                 if(fruits[rgt-1]==bskt1){
-                    int srink=rgt-1;
-                    while(srink>=0){
-                        if(fruits[srink]!=bskt1){
-                            break;
-                        }
-                        srink--;
-                    }
-                    lft=srink+1;
+                    left=Srink_window(rgt,fruits,bskt1);
                     bskt2=fruits[rgt];
                 }
                 else{
-                    int srink=rgt-1;
-                    while(srink>=0){
-                        if(fruits[srink]!=bskt2){
-                            break;
-                        }
-                        srink--;
-                    }
-                    lft=srink+1;
+                    left=Srink_window(rgt,fruits,bskt2);
                     bskt1=fruits[rgt];
                 }
             }
             rgt++;
         }
-        return Math.max(max,rgt-lft);
+        return Math.max(max,rgt-left);
+    }
+    public static int Srink_window(int rgt,int[] fruits,int bskt){
+        int srink=rgt-1;
+        while(srink>=0){
+            if(fruits[srink]!=bskt){
+                break;
+            }
+            srink--;
+        }
+        return srink+1;
+
     }
 }
