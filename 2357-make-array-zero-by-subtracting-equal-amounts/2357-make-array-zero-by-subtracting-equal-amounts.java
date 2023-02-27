97f8347ac145dcc6356043885a16d96f83a8866a
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-02-27 19:46:09 +0530

    Time: 1 ms (93.01%), Space: 40.5 MB (23.08%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -11,10 +11,10 @@ class Solution {
     public int minimumOperations(int[] nums) {
         int count=0;
         while(true){
-           if(minvalue(nums)==101){
+           int min=minvalue(nums);
+           if(min==101){
                 return count;
             } 
-            int min=minvalue(nums);
             count++;
             for(int i=0;i<nums.length;i++){
                 if(nums[i]!=0){
