d2c6166b3a4abb5e44aa9a588ae4e3df19fcfa74
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-02-27 19:52:27 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (21.89%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,27 +1,34 @@
 class Solution {
-    public static int minvalue(int[] nums){
-        int min=101;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0 && nums[i]<min){
-                min=nums[i];
-            }
-        }
-        return min;
-    }
+    // public static int minvalue(int[] nums){
+    //     int min=101;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]!=0 && nums[i]<min){
+    //             min=nums[i];
+    //         }
+    //     }
+    //     return min;
+    // }
     public int minimumOperations(int[] nums) {
-        int count=0;
-        while(true){
-           int min=minvalue(nums);
-           if(min==101){
-                return count;
-            } 
-            count++;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]!=0){
-                    nums[i]=nums[i]-min;
-                }
-            }
-        }
+        // int count=0;
+        // while(true){
+        //    int min=minvalue(nums);
+        //    if(min==101){
+        //         return count;
+        //     } 
+        //     count++;
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i]!=0){
+        //             nums[i]=nums[i]-min;
+        //         }
+        //     }
+        // }
         // return count;
+        
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        hs.remove(0);
+        return hs.size();
     }
 }
