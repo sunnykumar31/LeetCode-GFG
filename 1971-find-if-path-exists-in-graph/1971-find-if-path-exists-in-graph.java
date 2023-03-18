6b442dc85cc9b9079234e222c37523208af9e78e
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-03-18 19:54:05 +0530

    Time: 121 ms (63.69%), Space: 156 MB (38.70%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int src, int dst) {
-        HashMap<Integer,HashMap<Integer,Integer>> map=new HashMap<>();
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
         for(int i=0;i<n;i++){
-            map.put(i,new HashMap());
+            map.put(i,new ArrayList());
         }
         for(int i=0;i<edges.length;i++){
             int v1=edges[i][0];
             int v2=edges[i][1];
-            map.get(v1).put(v2,0);
-            map.get(v2).put(v1,0);
+            map.get(v1).add(v2);
+            map.get(v2).add(v1);
         }
         HashSet<Integer> visited=new HashSet<>();
         Queue<Integer> q=new LinkedList<>();
@@ -22,7 +22,7 @@ class Solution {
             if(rv==dst){
                 return true;
             }
-            for(int nbrs:map.get(rv).keySet()){
+            for(int nbrs:map.get(rv)){
                 if(!visited.contains(nbrs)) q.add(nbrs);
             }
         }
