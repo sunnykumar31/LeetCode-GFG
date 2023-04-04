f09eed7b258cecd897140032146886fd656cc3a0
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-04-04 08:14:31 +0530

    Time: 39 ms (36.13%), Space: 43.8 MB (27.33%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -3,14 +3,19 @@ class Solution {
         HashSet<Character> set=new HashSet<>();
         int count=1;
         for(int i=0;i<s.length();i++){
-            if(!set.contains(s.charAt(i))){
-                set.add(s.charAt(i));
-            }
-            else{
+            // if(!set.contains(s.charAt(i))){
+            //     set.add(s.charAt(i));
+            // }
+            // else{
+            //     count++;
+            //     set=new HashSet<>();
+            //     set.add(s.charAt(i));
+            // }
+            if(set.contains(s.charAt(i))){
                 count++;
                 set=new HashSet<>();
-                set.add(s.charAt(i));
             }
+            set.add(s.charAt(i));
         }
         return count;
     }
