9a38f58f617b381b0ce3d4f9f6ffd15cf65267a8
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-04-04 08:27:41 +0530

    Time: 16 ms (76.29%), Space: 42.8 MB (80.85%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,21 +1,35 @@
 class Solution {
     public int partitionString(String s) {
-        HashSet<Character> set=new HashSet<>();
+        //***********1st approch 39ms********
+        
+        // int count=1;
+        // HashSet<Character> set=new HashSet<>();
+        // for(int i=0;i<s.length();i++){
+        //     // if(!set.contains(s.charAt(i))){
+        //     //     set.add(s.charAt(i));
+        //     // }
+        //     // else{
+        //     //     count++;
+        //     //     set=new HashSet<>();
+        //     //     set.add(s.charAt(i));
+        //     // }
+        //     if(set.contains(s.charAt(i))){
+        //         count++;
+        //         set=new HashSet<>();
+        //     }
+        //     set.add(s.charAt(i));
+        // }
+        // return count;
+        
+        //******2nd approch by constant*****
         int count=1;
+        int freq[]=new int[26];
         for(int i=0;i<s.length();i++){
-            // if(!set.contains(s.charAt(i))){
-            //     set.add(s.charAt(i));
-            // }
-            // else{
-            //     count++;
-            //     set=new HashSet<>();
-            //     set.add(s.charAt(i));
-            // }
-            if(set.contains(s.charAt(i))){
+            if(freq[s.charAt(i)-'a']>0){
                 count++;
-                set=new HashSet<>();
+                Arrays.fill(freq,0);
             }
-            set.add(s.charAt(i));
+            freq[s.charAt(i)-'a']++;
         }
         return count;
     }
