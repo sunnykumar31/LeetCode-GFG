24d124b535f5cd02a76d2db66ed170a077dd4cb5
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-04-08 00:24:21 +0530

    Time: 3 ms (96.68%), Space: 42.6 MB (30.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,55 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       int dp[]=new int[nums.length];
-        Arrays.fill(dp,1);
+        // this is n^2 approch
+       // int dp[]=new int[nums.length];
+       //  Arrays.fill(dp,1);
+       //  for(int i=1;i<nums.length;i++){
+       //      // int max=0;
+       //      for(int j=i-1;j>=0;j--){
+       //          if(nums[i]>nums[j]){
+       //              dp[i]=Math.max(dp[i],dp[j]+1);
+       //          }
+       //      }
+       //      // dp[i]=max+dp[i];
+       //  }
+       //  int max=Integer.MIN_VALUE;
+       //  for(int i=0;i<dp.length;i++){
+       //      max=Math.max(max,dp[i]);
+       //  }
+       //  return max;
+        
+        //nlogn approch.
+        
+        int ar[]=new int[nums.length];
+        int l=1;
+        int count=1;
+        ar[0]=nums[0];
         for(int i=1;i<nums.length;i++){
-            // int max=0;
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
+            if(nums[i]>ar[l-1]){
+                ar[l++]=nums[i];
+                // System.out.println(ar[l-1]);
             }
-            // dp[i]=max+dp[i];
+            else{
+                int idx=search(ar,0,l-1,nums[i]);
+                ar[idx]=nums[i];
+                // System.out.println(ar[idx]);
+            }
         }
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<dp.length;i++){
-            max=Math.max(max,dp[i]);
-        }
-        return max;
+        return l;
     }
+    public static int search(int ar[],int si,int li,int target){
+        int ans=0;
+        while(si<=li){
+            int mid=(li+si)/2;
+            if(ar[mid]>=target){
+                ans=mid;
+                li=mid-1;
+            }
+            else{
+                si=mid+1;
+            }
+        }
+        return ans;
+    }
+    
 }
