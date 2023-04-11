0723f5ff724d54203382116dd2b51452143b6f91
sunny kumar <102225276+sunnykumar31@users.noreply.github.com>
2023-04-11 14:15:26 +0530

    Time: 278 ms (21.51%), Space: 42.8 MB (18.15%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,26 +2,22 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(word.charAt(0)==board[i][j] && search(board,word,i,j,0)){
-                    return true;
-                }
+                if(search(board,word,i,j,0)) return true;
             }
         }
         return false;
     }
+    static boolean f=false;
     public static boolean search(char[][] board, String word,int cr,int cc,int j){
-        if(j==word.length()){
-            return true;
-        }
-
-        if(cr<0 || cc<0 || cr>=board.length || cc>=board[0].length ||  board[cr][cc]!=word.charAt(j)){
-            return false;
-        }
+        if(j==word.length()) return true;
+        if(cr<0 || cc<0 || cr>=board.length || cc>=board[0].length ||  board[cr][cc]!=word.charAt(j)) return false;
         board[cr][cc]='0';
         int dx[]={1,-1,0,0};
         int dy[]={0,0,1,-1};
         for(int i=0;i<4;i++){
-            boolean ans=search(board,word,cr+dx[i],cc+dy[i],j+1);
+            int x=cr+dx[i];
+            int y=cc+dy[i];
+            boolean ans=search(board,word,x,y,j+1);
             if(ans) return ans;
         }
         board[cr][cc]=word.charAt(j);
